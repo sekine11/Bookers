3e9ca3352da3216f6104e240b21e1199b2d785a1
sekine11 <sekine11665@gmail.com>
2020-03-02 20:49:43 +0900

    [Start] 4-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,54 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	book = Book.new(book_params)
+  	if book.save
+  		flash[:notice] = "Book was successfully created."
+  		redirect_to book_path(book.id)
+  	else
+  		
+  	end
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	if book.update(book_params)
+  		flash[:notice] = "Book was successfully updated."
+  		redirect_to book_path(book.id)
+  	else
+  		flash[:notice] = "fail"
+  	end
+  end
+
+  def destroy
+  	@book = Book.find(params[:id])
+  	if @book.destroy
+  		flash[:notice] = "Book was successfully destroyed."
+  		redirect_to books_path
+  	else
+  		flash[:notice] = "fail"
+  		#リンクをいれる
+  	end
+  end
+
+  private
+
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
