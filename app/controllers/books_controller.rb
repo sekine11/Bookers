ff84129711b89ef993b578c2a863539afb3e07a2
sekine11 <sekine11665@gmail.com>
2020-03-03 22:06:05 +0900

    [Add] error message for edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,10 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	flash[:notice] = "Book was successfully updated."
-  	redirect_to book_path(book.id)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+    	flash[:notice] = "Book was successfully updated."
+    	redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -41,7 +44,6 @@ class BooksController < ApplicationController
   end
 
   private
-
   def book_params
   	params.require(:book).permit(:title, :body)
   end
